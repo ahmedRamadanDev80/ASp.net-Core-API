1f984532d66789f20a5e90225502723dce13a217
altair <ar80pro@gmail.com>
2023-03-21 22:08:05 +0200

    First API Endpoint - HTTPGET

--- learnApi/Controllers/VillaAPIController.cs
+++ learnApi/Controllers/VillaAPIController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using learnApi.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace learnApi.Controllers
@@ -7,5 +8,13 @@ namespace learnApi.Controllers
     [ApiController]
     public class VillaAPIController : ControllerBase
     {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas() 
+        {
+            return new List<Villa> { 
+                new Villa { Id = 1, Nmae = "Villa north coast" }, 
+                new Villa { Id = 2, Nmae = "Villa red sea" } 
+            };
+        }
     }
 }
---
+++ learnApi/Models/Villa.cs
@@ -0,0 +1,8 @@
+﻿namespace learnApi.Models
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public string Nmae { get; set; }
+    }
+}
