6efc5141e8a844e01e213c4ad489d9d9d3a64129
altair <ar80pro@gmail.com>
2023-03-21 22:36:14 +0200

    Get individual Villa

--- learnApi/Controllers/VillaAPIController.cs
+++ learnApi/Controllers/VillaAPIController.cs
@@ -15,5 +15,10 @@ namespace learnApi.Controllers
         {
             return VillaStore.villaList;
         }
+        [HttpGet("{id:int}")]
+        public VillaDTO GetVilla(int id) 
+        {
+            return VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+        }
     }
 }
