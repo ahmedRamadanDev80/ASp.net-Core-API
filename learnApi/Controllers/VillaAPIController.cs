88d9efb27d11ce9f10be96106141ff8abe8c286f
altair <ar80pro@gmail.com>
2023-03-22 15:27:14 +0200

    Status Code in Endpoints

--- learnApi/Controllers/VillaAPIController.cs
+++ learnApi/Controllers/VillaAPIController.cs
@@ -2,6 +2,7 @@
 using learnApi.Models;
 using learnApi.Models.Dto;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace learnApi.Controllers
@@ -11,14 +12,17 @@ namespace learnApi.Controllers
     public class VillaAPIController : ControllerBase
     {
         [HttpGet]
-        public IEnumerable<VillaDTO> GetVillas() 
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas() 
         {
-            return VillaStore.villaList;
+            return Ok(VillaStore.villaList);
         }
         [HttpGet("{id:int}")]
-        public VillaDTO GetVilla(int id) 
+        public ActionResult<VillaDTO> GetVilla(int id) 
         {
-            return VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+            if (id == 0) { return BadRequest(); }
+            var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+            if (villa == null) { return NotFound(); }
+            return Ok(villa);
         }
     }
 }
