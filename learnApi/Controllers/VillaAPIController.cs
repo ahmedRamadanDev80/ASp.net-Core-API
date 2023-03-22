48d19377d236273faa61ba7f6b7fb432c2e0bfd5
altair <ar80pro@gmail.com>
2023-03-22 15:48:02 +0200

    HttpPOST in Action

--- learnApi/Controllers/VillaAPIController.cs
+++ learnApi/Controllers/VillaAPIController.cs
@@ -17,6 +17,7 @@ namespace learnApi.Controllers
         {
             return Ok(VillaStore.villaList);
         }
+
         [HttpGet("{id:int}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -27,6 +28,23 @@ namespace learnApi.Controllers
             var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
             if (villa == null) { return NotFound(); }
             return Ok(villa);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status406NotAcceptable)]
+        public ActionResult<VillaDTO> CreateVilla([FromBody]VillaDTO villa)
+        {
+            if (villa == null) { return BadRequest(villa); }
+            if (villa.Id > 0)
+            {
+                return StatusCode(StatusCodes.Status406NotAcceptable);
+            }
+            villa.Id = VillaStore.villaList.OrderByDescending(u => u.Id).FirstOrDefault().Id + 1;
+            VillaStore.villaList.Add(villa);
+            return Ok(villa);
+
         }
     }
 }
