e981e2d4ee2702fa1b638acf0635237809848934
altair <ar80pro@gmail.com>
2023-03-26 16:56:16 +0200

    Base Service for API

---
+++ WebApp/Services/BaseService.cs
@@ -0,0 +1,76 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+using WebApp.Models;
+using WebApp.Services.IServices;
+using WebApp_Utility;
+
+namespace WebApp.Services
+{
+    public class BaseService : IBaseService
+    {
+        public APIResponse responseModel { get; set; }
+        public IHttpClientFactory httpClient { get; set; }
+
+        public BaseService(IHttpClientFactory httpClient)
+        {
+            this.responseModel = new();
+            this.httpClient = httpClient;
+        }
+
+        public async Task<T> SendAsync<T>(APIRequest apiRequest)
+        {
+            try 
+            {
+                //preparing the message we will send to the api
+                var client =httpClient.CreateClient("DaddyAPI");
+                HttpRequestMessage message = new HttpRequestMessage();
+                message.Headers.Add("Accept", "application/json");
+                message.RequestUri = new Uri(apiRequest.Url);
+
+                // in case of post or put we need to send data with the api request
+                if (apiRequest.Data != null)
+                {
+                    message.Content = new StringContent(JsonConvert.SerializeObject(apiRequest.Data),
+                        Encoding.UTF8, "application/json");
+                }
+
+                switch (apiRequest.apiType)
+                {
+                    case SD.ApiType.POST:
+                        message.Method = HttpMethod.Post;
+                        break;
+                    case SD.ApiType.PUT:
+                        message.Method = HttpMethod.Put;
+                        break;
+                    case SD.ApiType.DELETE:
+                        message.Method = HttpMethod.Delete;
+                        break;
+                    default:
+                        message.Method = HttpMethod.Get;
+                        break;
+                }
+                // making an object to catch the response of the api in it
+                HttpResponseMessage apiResponse = null;
+
+                // sending the message to the api
+                apiResponse = await client.SendAsync(message);
+
+                // parsing the api response
+                var apiContent = await apiResponse.Content.ReadAsStringAsync();
+                var APIResponse = JsonConvert.DeserializeObject<T>(apiContent);
+                return APIResponse;
+            }
+            catch (Exception e)
+            {
+                var dto = new APIResponse
+                {
+                    ErrorMessages = new List<string> { Convert.ToString(e.Message) },
+                    IsSuccess = false
+                };
+                var res = JsonConvert.SerializeObject(dto);
+                var APIResponse = JsonConvert.DeserializeObject<T>(res);
+                return APIResponse;
+            }
+        }
+    }
+}
---
+++ WebApp/Services/IServices/IBaseService.cs
@@ -0,0 +1,10 @@
+﻿using WebApp.Models;
+
+namespace WebApp.Services.IServices
+{
+    public interface IBaseService
+    {
+        APIResponse responseModel { get; set; }
+        Task<T> SendAsync<T>(APIRequest apiRequest);
+    }
+}
