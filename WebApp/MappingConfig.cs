4e13f1974968f79973a05c64d36d8f0ab76c17a1
altair <ar80pro@gmail.com>
2023-03-26 14:01:26 +0200

    AutoMapper Config

---
+++ WebApp/MappingConfig.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using WebApp.Models;
+using WebApp.Models.Dto;
+
+namespace WebApp
+{
+    public class MappingConfig :Profile
+    {
+        public MappingConfig()
+        {
+            CreateMap<VillaDTO, VillaCreateDTO>().ReverseMap();
+            CreateMap<VillaDTO, VillaUpdateDTO>().ReverseMap();
+
+            CreateMap<VillaNumberDTO, VillaNumberCreateDTO>().ReverseMap();
+            CreateMap<VillaNumberDTO, VillaNumberUpdateDTO>().ReverseMap();
+        }
+    }
+}
