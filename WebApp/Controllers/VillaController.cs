e838acb2f4a05bf238baf8ca559f2aa5ea3599d6
altair <ar80pro@gmail.com>
2023-03-27 17:26:55 +0200

    Calling Villa API

---
+++ WebApp/Controllers/VillaController.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WebApp.Models;
+using WebApp.Models.Dto;
+using WebApp.Services.IServices;
+
+namespace WebApp.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly IVillaService _villaService;
+        private readonly IMapper _mapper;
+        public VillaController(IVillaService villaService, IMapper mapper)
+        {
+            _villaService = villaService;
+            _mapper = mapper;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<VillaDTO> list = new();
+
+            var response = await _villaService.GetAllAsync<APIResponse>();
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<VillaDTO>>(Convert.ToString(response.Result));
+            }
+            return View(list);
+        }
+    }
+}
