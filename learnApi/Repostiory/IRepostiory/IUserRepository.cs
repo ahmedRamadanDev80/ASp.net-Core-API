b65dbe232ced75f9e059b7eb62c27fac5a6642e1
altair <ar80pro@gmail.com>
2023-03-30 20:10:31 +0200

    Add User Repository

---
+++ learnApi/Repostiory/IRepostiory/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using learnApi.Models.Dto;
+using learnApi.Models;
+
+namespace learnApi.Repostiory.IRepostiory
+{
+    public interface IUserRepository
+    {
+        bool IsUniqueUser(string username);
+        Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO);
+        Task<LocalUser> Register(RegisterationRequestDTO registerationRequestDTO);
+    }
+}
---
+++ learnApi/Repostiory/UserRepository.cs
@@ -0,0 +1,32 @@
+﻿using learnApi.Data;
+using learnApi.Models;
+using learnApi.Models.Dto;
+using learnApi.Repostiory.IRepostiory;
+
+namespace learnApi.Repostiory
+{
+    public class UserRepository : IUserRepository
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public UserRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public bool IsUniqueUser(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LocalUser> Register(RegisterationRequestDTO registerationRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
