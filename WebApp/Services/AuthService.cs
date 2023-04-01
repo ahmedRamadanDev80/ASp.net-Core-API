8f78befa36abfe42ccc805670e8ed33860fd49fd
altair <ar80pro@gmail.com>
2023-04-01 20:55:15 +0200

    Auth Service

---
+++ WebApp/Services/AuthService.cs
@@ -0,0 +1,30 @@
+﻿using WebApp.Models;
+using WebApp.Models.Dto;
+using WebApp.Services.IServices;
+using WebApp_Utility;
+
+namespace WebApp.Services
+{
+    public class AuthService : BaseService, IAuthService
+    {
+        private readonly IHttpClientFactory _clientFactory;
+        private string villaUrl;
+
+        public AuthService(IHttpClientFactory clientFactory, IConfiguration configuration) : base(clientFactory)
+        {
+            _clientFactory = clientFactory;
+            villaUrl = configuration.GetValue<string>("ServiceUrls:VillaAPI");
+
+        }
+
+        public Task<T> LoginAsync<T>(LoginRequestDTO obj)
+        {
+            return SendAsync<T>(new APIRequest(){ apiType = SD.ApiType.POST, Data = obj, Url = villaUrl + "/api/Users/login" });
+        }
+
+        public Task<T> RegisterAsync<T>(RegisterationRequestDTO obj)
+        {
+            return SendAsync<T>(new APIRequest(){ apiType = SD.ApiType.POST, Data = obj, Url = villaUrl + "/api/Users/register" });
+        }
+    }
+}
