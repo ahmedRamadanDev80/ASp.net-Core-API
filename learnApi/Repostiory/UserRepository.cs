7ed63a6451cd4760abbdeba24df5f811f3a39537
altair <ar80pro@gmail.com>
2023-04-06 00:09:04 +0200

    Login -Register in WebApp using Identity

--- learnApi/Repostiory/UserRepository.cs
+++ learnApi/Repostiory/UserRepository.cs
@@ -59,7 +59,7 @@ namespace learnApi.Repostiory
             {
                 Subject = new ClaimsIdentity(new Claim[]
                {
-                    new Claim(ClaimTypes.Name, user.Id.ToString()),
+                    new Claim(ClaimTypes.Name, user.UserName.ToString()),
                     new Claim(ClaimTypes.Role, roles.FirstOrDefault())
                }),
                 Expires = DateTime.UtcNow.AddDays(7),
@@ -71,7 +71,6 @@ namespace learnApi.Repostiory
             {
                 Token = tokenHandler.WriteToken(token),
                 User = _mapper.Map<UserDTO>(user),
-                Role = roles.FirstOrDefault(),
             };
             return loginResponseDTO;
 
